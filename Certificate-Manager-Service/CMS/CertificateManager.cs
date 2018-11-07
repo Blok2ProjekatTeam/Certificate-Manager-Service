825eb00e6ec8f9add2c83e85a44b5d0102ece1d7
Aleksandar Jančikin <apialeksandar@yahoo.com>
2018-11-07 10:51:51 +0100

    CheckValidation

--- Certificate-Manager-Service/CMS/CertificateManager.cs
+++ Certificate-Manager-Service/CMS/CertificateManager.cs
@@ -42,12 +42,12 @@ namespace CMS
 			{
 				if (sw.BaseStream.CanWrite)
 				{
-					sw.WriteLine(@"cd C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86");
+					sw.WriteLine(@"cd C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86");
 
 					sw.WriteLine("makecert -sv {0}.pvk -iv CMS.pvk -n \"CN = {1}\" -pe -ic CMS.cer {2}.cer -sr localmachine -ss My -sky exchange", certificate, certificate.ToLower(), certificate);
 
 					do
-					{
+                    { 
 						srvCert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, certificate);
 						
 					} while (srvCert == null);
@@ -55,13 +55,17 @@ namespace CMS
 					sw.WriteLine(" pvk2pfx.exe /pvk {0}.pvk /pi {1} /spc {2}.cer /pfx {3}.pfx",certificate,pass, certificate, certificate);
 
 					//	COPY CERTIFICATES
-					sw.WriteLine(@"cd C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86");
-					sw.WriteLine(@"copy {0}.cer C:\Users\Administrator\Desktop\CMS\Certificate-Manager-Service-master\Certificate-Manager-Service-master\Certificate-Manager-Service\Certificates", certificate);
-					sw.WriteLine(@"copy {0}.pfx C:\Users\Administrator\Desktop\CMS\Certificate-Manager-Service-master\Certificate-Manager-Service-master\Certificate-Manager-Service\Certificates", certificate);
-					sw.WriteLine(@"copy {0}.pvk C:\Users\Administrator\Desktop\CMS\Certificate-Manager-Service-master\Certificate-Manager-Service-master\Certificate-Manager-Service\Certificates", certificate);
+					sw.WriteLine(@"cd C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86");
+					sw.WriteLine(@"copy {0}.cer D:\FAX\IV godina\Blok 2\projekat\Certificate-Manager-Service\Certificate-Manager-Service\Certificates", certificate);
+					sw.WriteLine(@"copy {0}.pfx D:\FAX\IV godina\Blok 2\projekat\Certificate-Manager-Service\Certificate-Manager-Service\Certificates", certificate);
+					sw.WriteLine(@"copy {0}.pvk D:\FAX\IV godina\Blok 2\projekat\Certificate-Manager-Service\Certificate-Manager-Service\Certificates", certificate);
 
-					do
-					{
+                    sw.WriteLine("del {0}.cer", certificate);
+                    sw.WriteLine("del {0}.pfx", certificate);
+                    sw.WriteLine("del {0}.pvk", certificate);
+
+                    do
+                    {
 						srvCert2 = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, certificate);
 
 						if(srvCert2.HasPrivateKey)
@@ -77,5 +81,42 @@ namespace CMS
 			return ret;
 
 		}
+
+        public bool CheckValidation(string certificate)
+        {
+            bool ret = false;
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, certificate);
+
+            if(DateTime.Parse(cert.GetExpirationDateString()) < DateTime.Now)
+            {
+                ret = false;
+            }
+            else
+            {
+                ret = true;
+            }
+
+            if(ret)
+            {
+                if(!cert.IssuerName.Equals("CMS"))
+                {
+                    ret = false;
+                }
+            }
+
+            if (ret)
+            {
+                if (RevocationList.List.Contains(cert))
+                {
+                    ret = false;
+                }
+            }
+            else
+            {
+                RevocationList.List.Add(cert);
+            }
+
+            return ret;
+        }
 	}
 }
---
+++ Certificate-Manager-Service/CMS/RevocationList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    public class RevocationList
+    {
+        public static List<X509Certificate2> List = new List<X509Certificate2>();
+    }
+}
